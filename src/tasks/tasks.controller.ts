f8ed8229448b4d8ce95d7fa0752a9da2c6c2801a
Brendo Souza Pinheiro <brendo.spinheiro@gmail.com>
2021-03-02 22:13:11 -0300

    feat: add create task method and task dto

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get } from '@nestjs/common';
 import { TasksService } from './tasks.service';
 import { Task } from './task.entity';
 
--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,11 +1,13 @@
 import { Injectable } from '@nestjs/common';
 import { Task, TaskStatus } from './task.entity';
+import { v4 as uuid } from 'uuid';
+import { CreateTaskDto } from './task.dto';
 
 @Injectable()
 export class TasksService {
   private tasks: Task[] = [
     {
-      id: '1',
+      id: uuid(),
       title: 'Estudar NestJS',
       description: 'Preciso estudar nest e ser foda com esse framework',
       status: TaskStatus.OPEN,
@@ -14,5 +16,18 @@ export class TasksService {
 
   getAllTasks(): Task[] {
     return this.tasks;
+  }
+
+  createTask({ title, description }: CreateTaskDto): Task {
+    const newTask: Task = {
+      id: uuid(),
+      title,
+      description,
+      status: TaskStatus.OPEN,
+    }
+
+    this.tasks.push(newTask);
+
+    return newTask;
   }
 }
