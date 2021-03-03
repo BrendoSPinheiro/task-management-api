12eb2757cf5e3190677c93a5a4ed9d8a67ec1212
Brendo Souza Pinheiro <brendo.spinheiro@gmail.com>
2021-03-02 21:46:16 -0300

    feat: add get all tasks method

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,7 +1,15 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 import { TasksService } from './tasks.service';
+import { Task } from './task.entity';
 
 @Controller('tasks')
 export class TasksController {
   constructor(private tasksService: TasksService) {}
+
+  @Get()
+  index(): Task[] {
+    const tasks = this.tasksService.getAllTasks();
+
+    return tasks;
+  }
 }
--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,4 +1,18 @@
 import { Injectable } from '@nestjs/common';
+import { Task, TaskStatus } from './task.entity';
 
 @Injectable()
-export class TasksService {}
+export class TasksService {
+  private tasks: Task[] = [
+    {
+      id: '1',
+      title: 'Estudar NestJS',
+      description: 'Preciso estudar nest e ser foda com esse framework',
+      status: TaskStatus.OPEN,
+    }
+  ];
+
+  getAllTasks(): Task[] {
+    return this.tasks;
+  }
+}
