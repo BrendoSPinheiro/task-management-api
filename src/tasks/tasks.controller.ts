83072fcca28982d4b3ce6aba10b73eab196bcdd3
Brendo Souza Pinheiro <brendo.spinheiro@gmail.com>
2021-03-02 22:30:33 -0300

    feat: add create task method in task controller

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,6 +1,7 @@
-import { Body, Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { TasksService } from './tasks.service';
 import { Task } from './task.entity';
+import { CreateTaskDto } from './task.dto';
 
 @Controller('tasks')
 export class TasksController {
@@ -11,5 +12,12 @@ export class TasksController {
     const tasks = this.tasksService.getAllTasks();
 
     return tasks;
+  }
+
+  @Post()
+  store(@Body() { title, description }: CreateTaskDto): Task {
+    const task = this.tasksService.createTask({title, description});
+
+    return task;
   }
 }
